6bafa3c53a2f59f8550bffd25614e13536113151
Administrator <243620922@qq.com>
2020-09-07 15:34:57 +0800

    commit --- gitee

--- src/main/java/com/atguigu/test/Demo.java
+++ src/main/java/com/atguigu/test/Demo.java
@@ -7,5 +7,8 @@ package com.atguigu.test;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("我是大数据工程师，没有两万别喊我！");
+        // 将不需要的文件过滤不提交到远程仓库！
+        // idea 设置
+        //*.cmd;*.gitignore;*.hprof;*.iml;*.mvn;*.pyc;*.pyo;*.rbc;*.yarb;*~;.DS_Store;.git;.hg;.idea;.svn;CVS;__pycache__;_svn;mvnw;target;vssver.scc;vssver2.scc;
     }
 }
---
+++ src/main/java/com/atguigu/test/TestDemo.java
@@ -0,0 +1,8 @@
+package com.atguigu.test;
+
+/**
+ * @author mqx
+ * @date 2020-9-7 15:33:17
+ */
+public class TestDemo {
+}
