4adf0c844f749e53b43b664b6033470b41879518
Administrator <243620922@qq.com>
2020-09-07 15:27:20 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/test/Demo.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @author mqx
+ * @date 2020-9-7 15:18:38
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("我是大数据工程师，没有两万别喊我！");
+    }
+}
