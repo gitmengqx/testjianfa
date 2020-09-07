7d2569ab67d78e71fde0443d3a4017584147101f
Administrator <243620922@qq.com>
2020-09-07 16:51:11 +0800

    commit --- test---0000

--- src/main/java/com/atguigu/test/Demo.java
+++ src/main/java/com/atguigu/test/Demo.java
@@ -9,6 +9,6 @@ public class Demo {
         System.out.println("我是大数据工程师，没有两万别喊我！");
         // 将不需要的文件过滤不提交到远程仓库！
         // idea 设置
-        //*.cmd;*.gitignore;*.hprof;*.iml;*.mvn;*.pyc;*.pyo;*.rbc;*.yarb;*~;.DS_Store;.git;.hg;.idea;.svn;CVS;__pycache__;_svn;mvnw;target;vssver.scc;vssver2.scc;
+        // *.cmd;*.gitignore;*.hprof;*.iml;*.mvn;*.pyc;*.pyo;*.rbc;*.yarb;*~;.DS_Store;.git;.hg;.idea;.svn;CVS;__pycache__;_svn;mvnw;target;vssver.scc;vssver2.scc;
     }
 }
